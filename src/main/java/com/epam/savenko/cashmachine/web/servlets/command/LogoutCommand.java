ca646392a1c6fb6608bd65397a2c6bcb90859078
Oleh Savenko <osavenko@gmail.com>
2021-09-09 12:16:08 +0300

    add file tag logout.jspf and command LogoutCommand.java

--- src/main/java/com/epam/savenko/cashmachine/web/servlets/command/LogoutCommand.java
+++ src/main/java/com/epam/savenko/cashmachine/web/servlets/command/LogoutCommand.java
@@ -1,13 +1,28 @@
 package com.epam.savenko.cashmachine.web.servlets.command;
 
+import com.epam.savenko.cashmachine.web.Path;
+import org.apache.log4j.Logger;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 public class LogoutCommand extends Command {
+
+    private static final Logger LOG = Logger.getLogger(LogoutCommand.class);
+
     @Override
     public String execute(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
-        return null;
+        LOG.debug("Command logout starts");
+
+        HttpSession session = req.getSession(false);
+        if (session != null) {
+            session.invalidate();
+        }
+
+        LOG.debug("Command logout finished");
+        return Path.PAGE_LOGIN;
     }
 }
