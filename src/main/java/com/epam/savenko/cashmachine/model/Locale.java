4abb6f9962e17b20b02c403412fa477b581f1c4a
Oleh Savenko <osavenko@gmail.com>
2021-09-18 21:39:30 +0300

    Add javadoc to class Locale

--- src/main/java/com/epam/savenko/cashmachine/model/Locale.java
+++ src/main/java/com/epam/savenko/cashmachine/model/Locale.java
@@ -3,21 +3,55 @@ package com.epam.savenko.cashmachine.model;
 import java.io.Serializable;
 import java.util.Objects;
 
+/**
+ * Class describes locales in application.
+ *
+ * @author Oleh Savenko
+ * @version 1.0
+ */
 public class Locale implements Serializable {
 
     private static final long serialVersionUID = -8052717055666149796L;
-
+    /**
+     * field id
+     */
     private int id;
+    /**
+     * field name
+     */
     private String name;
+    /**
+     * filed description
+     */
     private String description;
 
+    /**
+     * Initializes a newly created Locale object.
+     * @see Locale#Locale(String, String)
+     * @see Locale#Locale(int, String, String)
+     */
     public Locale() {
     }
 
+    /**
+     * Initializes a newly created Locale object. Sets two parameters <b>name</b>, <b>description</b>
+     * @param name - locale name
+     * @param description - locale description
+     * @see Locale#Locale()
+     * @see Locale#Locale(int, String, String)
+     */
     public Locale(String name, String description) {
         this.name = name;
         this.description = description;
     }
+    /**
+     * Initializes a newly created Locale object. Sets three parameters <b>id</b>, <b>name</b>, <b>description</b>
+     * @param id - locale id
+     * @param name - locale name
+     * @param description - locale description
+     * @see Locale#Locale()
+     * @see Locale#Locale(String, String)
+     */
 
     public Locale(int id, String name, String description) {
         this.id = id;
@@ -25,26 +59,50 @@ public class Locale implements Serializable {
         this.description = description;
     }
 
+    /**
+     * Gets value field {@link Locale#id}
+     * @return id locale (value {@link Locale#id})
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * Sets value field {@link Locale#id}
+     * @param id - locale id
+     */
     public void setId(int id) {
         this.id = id;
     }
 
+    /**
+     * Gets short name locale like <b>ua</b>, <b>ru</b>... This function gets value field {@link Locale#name}
+     * @return short name locale
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Sets short name locale like <b>ua</b>, <b>ru</b>...
+     * @param name - short name locale
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Gets description locale
+     * @return description locale
+     */
     public String getDescription() {
         return description;
     }
 
+    /**
+     * Sets description locale
+     * @param description - description locale
+     */
     public void setDescription(String description) {
         this.description = description;
     }
