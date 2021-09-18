1a5dcbc1d50ea0a2690ef7c1f633fe446e1b3e69
Oleh Savenko <osavenko@gmail.com>
2021-09-18 22:50:20 +0300

    Add javadoc to class Role

--- src/main/java/com/epam/savenko/cashmachine/model/Role.java
+++ src/main/java/com/epam/savenko/cashmachine/model/Role.java
@@ -3,37 +3,90 @@ package com.epam.savenko.cashmachine.model;
 import java.io.Serializable;
 import java.util.Objects;
 
+/**
+ * The class describe roles for application users
+ *
+ * @author Oleg Savenko
+ * @version 1.0
+ */
 public class Role implements Serializable {
 
     private static final long serialVersionUID = 1375413957481220091L;
 
+    /**
+     * field id
+     */
     private int id;
+    /**
+     * field name
+     */
     private String name;
 
+    /**
+     * Initializes a newly created Role object.
+     *
+     * @see Role#Role(String)
+     * @see Role#Role(int, String)
+     */
     public Role() {
     }
 
+    /**
+     * Initializes a newly created Role object with <b>name</b>.
+     *
+     * @param name - name of role
+     * @see Role#Role()
+     * @see Role#Role(int, String)
+     */
     public Role(String name) {
         this.name = name;
     }
 
+    /**
+     * Initializes a newly created Role object with two parameters <b>id</b>, <b>name</b>.
+     *
+     * @param id   - id role
+     * @param name - name of role
+     * @see Role#Role()
+     * @see Role#Role(String)
+     */
     public Role(int id, String name) {
         this.id = id;
         this.name = name;
     }
 
+    /**
+     * Gets id role. This is field {@link Role#id}.
+     *
+     * @return id role
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * Sets id role. This is field {@link Role#id}.
+     *
+     * @param id
+     */
     public void setId(int id) {
         this.id = id;
     }
 
+    /**
+     * Gets name of role. This is field {@link Role#name}.
+     *
+     * @return name of role
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Sets name of role. This is field {@link Role#name}.
+     *
+     * @param name - name of role
+     */
     public void setName(String name) {
         this.name = name;
     }
